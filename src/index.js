ba267ffe38ab77b3ff9a1a6870acbd06bae26f1e
Oksana Kharun <oksana0202g@gmail.com>
2023-11-06 20:19:37 +0000

    update

--- src/index.js
+++ src/index.js
@@ -22,27 +22,22 @@ let totalHits = 0;
     captionsData: 'alt', 
     captionPosition: 'bottom', 
     captionDelay: 250, 
- });
-  
-
+  });
 
 async function onSearch(event) {
     event.preventDefault();
      formValue = formEl.elements.searchQuery.value;
-  if (formValue.length == 0)
-    return alert('Форма не може бути пуста');
+    if (formValue.length == 0) return alert('Форма не може бути пуста');
   resultPage = 1;
   arrayDataLength = 0;
   totalHits = 0;
-  await getSearch(formValue, resultPage)
-    .then(data => {
-      const arrayData = data.hits;
+    await getSearch(formValue, resultPage).then(data => {
+      const arrayData = data.data.hits;
       arrayDataLength += arrayData.length;
-      totalHits = data.totalHits;
+      totalHits = data.data.totalHits;
       if (arrayData.length == 0)
         return Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.");
-    
-      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
       createDomEl(arrayData);
       if (arrayDataLength >= totalHits) {
         buttonLoadEl.classList.add('is-hidden');
@@ -65,8 +60,7 @@ buttonLoadEl.addEventListener('click', onLoader);
 function test(arrayData) {
  return arrayData.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
     `<div class="photo-card">
-        <a class="gallery__link" href="${largeImageURL}">
-        <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+        <a class="gallery__link" href="${largeImageURL}"  ><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
   <div class="info">
     <p class="info-item">
       <b>likes:${likes}</b>
@@ -92,9 +86,8 @@ function createDomEl(arrayData) {
 
 async function onLoader() {
     resultPage += 1;
-  await getSearch(formValue, resultPage)
-    .then(data => {
-      const arrayData = data.hits;
+    await getSearch(formValue, resultPage).then(data => {
+      const arrayData = data.data.hits;
       arrayDataLength += arrayData.length;
       createNewDomEl += test(arrayData);
       galleryEl.innerHTML = createNewDomEl;
@@ -104,12 +97,4 @@ async function onLoader() {
         Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
       } 
     })
-}
-
-function scrollToNextGroup() {
-    const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
-    window.scrollBy({
-        top: cardHeight,
-        behavior: "smooth",
-    });
 }
